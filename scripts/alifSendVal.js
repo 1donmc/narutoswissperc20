e924f4dbdb5ffc21953897c29ed910b47533f190
intellygentle <alimisheriff255@gmail.com>
2024-08-02 02:24:57 -0700

    Add all project files

---
+++ scripts/alifSendVal.js
@@ -0,0 +1,32 @@
+const hre = require("hardhat");
+const { sendSignedShieldedQuery } = require("./utils");
+
+const PK = "go-find-it";
+const deployedContractAddress = "0xA49e2928daaa19e2aDa427BaB3F6A1AbDad214B2";
+
+async function main() {
+  const PERC20 = await hre.ethers.getContractFactory("PERC20Sample");
+  const perc20 = PERC20.attach(deployedContractAddress);
+
+  const provider = new hre.ethers.providers.JsonRpcProvider(hre.network.config.url);
+  const wallet = new hre.ethers.Wallet(PK, provider);
+
+  const tx = await wallet.sendTransaction({
+    to: perc20.address,
+    value: 100
+  });
+  await tx.wait();
+  console.log(tx);
+
+  let encodedFunctionData = perc20.interface.encodeFunctionData("balanceOf", [wallet.address]);
+  let req = await sendSignedShieldedQuery(wallet, perc20.address, encodedFunctionData);
+
+  let balance = perc20.interface.decodeFunctionResult("balanceOf", req)[0];
+  console.log('Balance: ', balance.toString());
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
+
